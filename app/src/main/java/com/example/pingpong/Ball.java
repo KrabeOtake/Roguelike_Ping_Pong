3240a7cebc5f2f125095de155adcbcea4ca98556
Sergey Pinkevich <sergeipinkevich163@gmail.com>
2016-07-18 13:44:50 +0300

    add difficulty transfer

---
+++ app/src/main/java/com/example/pingpong/Ball.java
@@ -0,0 +1,8 @@
+package com.example.pingpong;
+
+/**
+ * Created by Сергей Пинкевич on 18.07.2016.
+ */
+
+public class Ball {
+}
--- app/src/main/java/com/example/pingpong/GameActivity.java
+++ app/src/main/java/com/example/pingpong/GameActivity.java
@@ -1,15 +1,25 @@
 package com.example.pingpong;
 
+import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class GameActivity extends AppCompatActivity {
 
+    private static final String DIFFICULTY = "difficulty";
     private static int gameDifficulty;
+    // 1 - easy
+    // 2 - medium
+    // 3 - hard
+    // Value, which was sent by default = 2, medium
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        Intent intent = getIntent();
+        gameDifficulty = intent.getIntExtra(DIFFICULTY, 2); // 2 is value by default (medium level)
     }
 }
